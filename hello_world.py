5a196fe716502f6223b2b2e8b69e6664dc518ff1
Jasmine Cohen <jasmoz@gmail.com>
2025-03-27 17:01:21 +0200

    added 2 more languages

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,9 @@
-# print "hello world" to the console in 3 languages
+# print "hello world" to the console in 5 languages
 print("Hello World")
 print("Hola Mundo")
 print("Bonjour le monde")
+print("Hallo Welt")
+print("Ciao Mondo")
+
+
 
